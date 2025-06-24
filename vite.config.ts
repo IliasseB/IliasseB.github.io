12d56e5fd8d5001632829f5f54d2559086d0acef
IliasseB <iliasse.bellouch@edu.univ-paris13.fr>
2025-06-24 21:11:21 +0200

    L

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // <-- très important pour GitHub Pages à la racine
-  plugins: [react()]
-})
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
